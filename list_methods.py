713c358a2a10ade8ce376c7561bf5dc5b10ce139
Jonathan M Paduthala <jonathanpaduthala@gmail.com>
2024-07-07 00:23:10 +0530

    File consists of list  methods regularly used in python

---
+++ list_methods.py
@@ -0,0 +1,28 @@
+#List Methods
+a=[2,1,3]
+a.append(4) #Used to add a element to the end of a list
+print(a)
+
+
+a.sort() # used to sort the list in ascending order
+print(a)
+
+
+a.sort(reverse=True) # used to sort in descending order
+print(a)
+
+a.reverse() # used to reverse the order of elements in the list
+print(a)
+
+a.insert(4,5)  # used to insert a value at the inx syntax list_name.insert(indx,val)
+print(a)
+
+a.insert(4,8)  # repeating the function pushes the new value at the same position and the rest of elements to the right
+print(a)
+
+a.remove(8) # used to remove specific element from the list
+print(a)
+
+a.pop(2) # used to remove the element at the idx position
+
+
