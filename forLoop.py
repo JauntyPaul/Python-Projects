84689ed3a4bb9c1fff0f2a2e8516e2bf7f282296
Jaunty Paul <jonathanpaduthala@gmail.com>
2024-07-17 00:16:36 +0530

    Leant range and pass, also solved few practice questions of Loop

--- forLoop.py
+++ forLoop.py
@@ -21,3 +21,43 @@ for char in str:
         break
 else:
     print("char not Found")
+    
+    
+print("\n")
+
+# Range in Python
+
+seq = range(5) # Gives a sequence of numbers starting from 0 to End index -1
+for i in seq:
+    print(i)
+    
+# Above command can be written in more simpler way
+for i in range(10):
+    print(i)
+print("\n")    
+# Generic syntax is range(start,stop,step)
+
+# WAP to print all even number from 1 to 100
+print("Even Numbers")
+for i in range(2,101,2):
+    print(i)
+    
+print("\n")
+
+# Pass Command
+for i in range(5):
+    pass            # It is a null statement that does simply nothing, just like you.
+                    # Used for future hold, portion of code which is handled later on.
+                    
+# LOOP PRACTICE PROGRAMS
+
+
+# WAP to find the factorial of a number
+num = int(input("Enter the no. "))
+fact = 1
+i=1
+while i<=num:
+    for i in range(1,num+1):
+        fact *=i
+        i += 1
+print("Fatorial of ",num,"is:",fact)
