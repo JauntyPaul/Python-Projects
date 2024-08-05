d90a09be3af9b970f823036efa45bb80a6707f31
Jaunty Paul <jonathanpaduthala@gmail.com>
2024-08-05 23:53:03 +0530

    Finished the second question

--- oopsPart2Practice.py
+++ oopsPart2Practice.py
@@ -15,3 +15,26 @@ class Circle:
 c1=Circle(5)
 print("The area is ",c1.area())
 print("The perimeter is ",c1.perimeter())
+
+#############################################################################################################
+
+print("\n")
+#Q2. Class Employee, attr-> role, dept, sal , methods-> show details,
+#       class Engineer that inherits from Employee has extra attr name and age
+
+class Employee:
+    def __init__(self,role,dept,sal):
+        self.role=role
+        self.dept=dept
+        self.sal=sal
+    
+    def showdetails(self):
+        print("Role is ",self.role)
+        print("Dept is ",self.dept)
+        print("Salary is ",self.sal)
+        
+# class Engineer(Employee):
+    # def __init__(self, role, dept, sal,name,role):
+        
+emp1=Employee("Rep","CS",85000)
+emp1.showdetails()
