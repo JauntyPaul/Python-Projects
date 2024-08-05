a49b26b5c0d247becc8bf37e028cf9c8176ab77a
Jaunty Paul <jonathanpaduthala@gmail.com>
2024-08-06 00:39:17 +0530

    Finished OOPS in Python part 2

--- oopsPart2Practice.py
+++ oopsPart2Practice.py
@@ -33,8 +33,44 @@ class Employee:
         print("Dept is ",self.dept)
         print("Salary is ",self.sal)
         
-# class Engineer(Employee):
-    # def __init__(self, role, dept, sal,name,role):
+class Engineer(Employee):
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+        super().__init__("Engineer","CS","1,00,000")
+
         
 emp1=Employee("Rep","CS",85000)
 emp1.showdetails()
+
+engg1=Engineer("John",45)
+engg1.showdetails()
+
+
+##################################################################################################
+
+
+print("\n")
+
+
+#Q3. Class order which has the items and prices
+
+class Order:
+    def __init__(self,item,price):
+        self.item=item
+        self.price=price
+        
+    def __gt__(self,odr2):
+        if(self.price>ord2.price):
+            print("Order 1 > Order 2")
+        elif(self.price<ord2.price):
+            print("Order 2 > Order 1")
+        
+ord1=Order("Chips",20)
+ord2=Order("Pepsi",45)
+
+ord1.__gt__(ord2)
+# Here above the __gt__ is a dunder function, so direct calling odr1 > ord2 also works 
+
+ord1>ord2
+
